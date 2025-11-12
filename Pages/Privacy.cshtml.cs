aa38beddaa39ebdd51aa2a489b97b8d272114e97
samyv <samyv@SAMY-DUDU>
2025-11-12 11:47:33 +0200

    Add project files.

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Vlonga_Samuel_Lab1.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
